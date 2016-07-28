15a92a6317e3960d07c894d54ad6291110bd2c53
PaulPham <p.pham204@gmail.com>
2016-07-27 21:31:31 -0700

    mailing list

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,10 +1,29 @@
 class VisitorsController < ApplicationController
 
   def new
-    @owner = Owner.new
-    # flash.now[:notice] = 'Welcome!'
-    # flash.now[:alert] = "My birthday is soon."
-    # render 'visitors/new'
+    @visitor = Visitor.new
   end
 
+  def create
+    @visitor = Visitor.new(secure_params)
+    if @visitor.valid?
+      @visitor.subscribe
+      flash[:notice] = "Signed up #{@visitor.email}."
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def secure_params
+    params.require(:visitor).permit(:email)
+  end
+  
 end
+
+#@owner = Owner.new
+# flash.now[:notice] = 'Welcome!'
+# flash.now[:alert] = "My birthday is soon."
+# render 'visitors/new'
